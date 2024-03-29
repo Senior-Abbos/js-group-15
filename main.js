eeb9dd71600663349e10dde1918ec065b47dd549
abbos-khamidov <khamidov3538@gmail.com>
2024-03-29 15:25:08 +0500

    начало функций и параметры

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 // Новая тема функции
 
+// function functionName(parameters) {
+//      code
+// }
+
+// functionName()
+// С одним параметром
+// function sayHello(name) {
+//     console.log(`Hello, ${name}`)
+//     // console.log('Hello', name, 'welcome')
+//     // console.log(`Hello mr ${name} welcome to an app`)
+// }
+
+
+
+// с 2 параметрами
+function sayHello(name, age) {
+    console.log(`Здравствуйте, ${name}, получается вам ${age} лет`)
+}
+
+sayHello('Samina', 15)
+sayHello('Karina', 14)
+sayHello('Vera', 16)
