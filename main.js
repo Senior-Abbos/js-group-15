9d178794252593db8b9447b928ac0a44e9fe82f1
abbos-khamidov <khamidov3538@gmail.com>
2024-03-29 16:00:09 +0500

    калькулятор с функцией

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-// Новая тема функции
+// // Новая тема функции
 
-// function functionName(parameters) {
-//      code
+// // function functionName(parameters) {
+// //      code
+// // }
+
+// // functionName()
+// // С одним параметром
+// // function sayHello(name) {
+// //     console.log(`Hello, ${name}`)
+// //     // console.log('Hello', name, 'welcome')
+// //     // console.log(`Hello mr ${name} welcome to an app`)
+// // }
+
+
+
+// // с 2 параметрами
+// function sayHello(name, age) {
+//     console.log(`Здравствуйте, ${name}, получается вам ${age} лет`)
 // }
 
-// functionName()
-// С одним параметром
-// function sayHello(name) {
-//     console.log(`Hello, ${name}`)
-//     // console.log('Hello', name, 'welcome')
-//     // console.log(`Hello mr ${name} welcome to an app`)
+// sayHello('Samina', 15)
+// sayHello('Karina', 14)
+// sayHello('Vera', 16)
+
+
+// function calculator(num1, num2, symbol) {
+//     if (symbol == '+') {
+//         console.log(num1 +num2)
+//     }
 // }
 
+// calculator(10,20, '+')
 
-
-// с 2 параметрами
-function sayHello(name, age) {
-    console.log(`Здравствуйте, ${name}, получается вам ${age} лет`)
-}
-
-sayHello('Samina', 15)
-sayHello('Karina', 14)
-sayHello('Vera', 16)
+// let sum = function(a, b) => a + b
